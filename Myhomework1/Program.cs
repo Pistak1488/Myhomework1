dff16cafcd59f923d73f795008a5e1a0df54dba1
Пистолетов Александр <Lutcifer333@gmail.com>
2022-05-04 13:02:00 +0700

    Добавьте файлы проекта.

---
+++ Myhomework1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Myhomework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello my ferst hmework on C#!");
+            string input = Console.ReadLine();
+        }
+    }
+}
